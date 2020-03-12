39318069e8cb945386ef16ab5f6c48aaa92d7754
Andi Muh Ridhal A <47064574+iather@users.noreply.github.com>
2020-03-12 22:04:35 +0700

    TUGAS 1 [M2]-PBO
    TUGAS 1
    ■ Buat 3 Class dengan ketentuan sebagai berikut : 1) Buatlah sebuah aplikasi dalam bahasa java yang akan menampilkan teks : “ Selamat Belajar ” 2) Buatlah sebuah aplikasi dalam bahasa java yang akan menampilkan teks : “ Belajar Bahasa Pemrograman Java ” 3) Buatlah sebuah aplikasi dalam bahasa java yang akan menampilkan teks : “ Selesai Belajar Pemrograman Java” ■ Upload Tugas Program di akun github masing-masing. Atur data masingmasing github sehingga dapat dicari lebih mudah.. ■ Share link github masing-masing melalui grup

---
+++ bolehlahh.java
@@ -0,0 +1,5 @@
+public class bolehlahh{
+	public static void main (String[] args){
+		System.out.println("Selamat Belajar");
+	}
+}
---
+++ bolehlahh2.java
@@ -0,0 +1,5 @@
+public class bolehlahh2{
+	public static void main (String[] args){
+		System.out.println("Selesai Belajar Pemrograman Java");
+	}
+}
